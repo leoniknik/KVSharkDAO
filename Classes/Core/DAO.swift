ac11f8b2b18f0042f5f937fcdf8e96a8715ce66e
VolodinK <VolodinK@binbank.ru>
2018-02-26 11:29:19 +0300

    добавлен родительский класс дао

--- Classes/Core/DAO.swift
+++ Classes/Core/DAO.swift
@@ -10,6 +10,7 @@ import Foundation
 
 public protocol DAOProtocol {
     
+//    associatedtype Entry: EntryProtocol
     associatedtype Entry
     associatedtype Entity: EntityProtocol
     associatedtype Translator: TranslatorProtocol
@@ -29,3 +30,48 @@ public protocol DAOProtocol {
     func erase() -> Bool
     
 }
+
+public class DAO<Entity: EntityProtocol> {
+    
+    private var persistEntity: ((Entity) -> Bool)
+    private var persistEntities: (([Entity]) -> Bool)
+    private var readById: ((Int) -> Entity?)
+    private var readAll: (() -> [Entity])
+    private var eraseById: ((Int) -> Bool)
+    private var eraseAll: (() -> Bool)
+    
+    
+    public init<DAO: DAOProtocol>(_ dao: DAO) where DAO.Entity == Entity {
+        persistEntity = dao.persist
+        persistEntities = dao.persist
+        readById = dao.read(id:)
+        readAll = dao.read
+        eraseById = dao.erase(id:)
+        eraseAll = dao.erase
+    }
+    
+    @discardableResult public func persist(entity: Entity) -> Bool {
+        return persistEntity(entity)
+    }
+    
+    @discardableResult public func persist(entities: [Entity]) -> Bool {
+        return persistEntities(entities)
+    }
+    
+    public func read(id: Int) -> Entity? {
+        return readById(id)
+    }
+    
+    public func read() -> [Entity] {
+        return readAll()
+    }
+    
+    @discardableResult public func erase(id: Int) -> Bool {
+        return eraseById(id)
+    }
+    
+    @discardableResult public func erase() -> Bool {
+        return eraseAll()
+    }
+}
+
